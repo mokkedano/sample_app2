afed6ac47f672d7900e3c48af97e9b26d1ad3a84
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-04-04 13:35:30 +0000

    [Add] sample_app2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'homes#top'
+  resources :lists
 
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
